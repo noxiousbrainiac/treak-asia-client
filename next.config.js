10bd276199c3fcb42a2167233c56fb354a872f7f
Shabdan <shabdan@mancho.dev>
2022-11-08 15:28:43 +0600

    updated commits

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	i18n: {
+		locales: ['fr', 'en'],
+		defaultLocale: 'en'
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
