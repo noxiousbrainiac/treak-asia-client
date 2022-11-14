8168d5bba2eae9eae766b28c9e0ff1a4915859e8
Shabdan <shabdan@mancho.dev>
2022-11-15 00:11:28 +0600

    updated index page

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import styles from '../app/utils/styles/Home.module.scss';
+import client from '../app/utils/contentful';
+
+export default function Home({ locations }) {
+	console.log(locations);
+	return <div className={styles.container}></div>;
+}
+
+export async function getStaticProps({ locale }) {
+	const locations = await client.getEntries({
+		content_type: 'location',
+		locale
+	});
+
+	return {
+		props: { locations }
+	};
+}
