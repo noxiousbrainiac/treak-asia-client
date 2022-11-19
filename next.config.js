8149dcf474cd041963dba2e54f9e76c38b5e91c8
Shabdan <shabdan@mancho.dev>
2022-11-20 00:16:09 +0600

    updated

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ['images.ctfassets.net'],
-		loader: 'custom',
-		path: '/_next/image',
+		domains: ['images.ctfassets.net', 'www.kirghizasia.com'],
 		deviceSizes: [390, 450, 568, 768, 1024, 1200, 1440, 1920, 2560, 3840],
-		formats: ['image/webp']
+		formats: ['image/webp', 'image/avif']
 	},
 	i18n: {
 		locales: ['fr', 'en'],
--- utils/lists/lists.js
+++ utils/lists/lists.js
@@ -1,10 +1,12 @@
 export const navMenuList = {
 	fr: [
 		{ title: 'Accueil', url: '/' },
-		{ title: 'Contact', url: '/contact' }
+		{ title: 'Contact', url: '/contact' },
+		{ title: 'About us', url: '/about-us' }
 	],
 	en: [
 		{ title: 'Home', url: '/' },
-		{ title: 'Contact', url: '/contact' }
+		{ title: 'Contact', url: '/contact' },
+		{ title: 'About us', url: '/about-us' }
 	]
 };
