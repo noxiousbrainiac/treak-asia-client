3108b64b6dd4cf95d0f2b0ed96f184a6d6af55a1
Shabdan <shabdan@mancho.dev>
2022-11-21 21:43:24 +0600

    added new section

--- components/screens/PageContent/index.js
+++ components/screens/PageContent/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getDocumentFields } from 'utils/contentful/helper';
 import SectionRichText from 'components/ui/Sections/SectionRichText';
+import SectionProfilesCards from '../../ui/Sections/SectionProfilesCards';
 
 const PageContent = ({ sectionData }) => {
 	const { type, data, id } = getDocumentFields(sectionData);
@@ -8,7 +9,8 @@ const PageContent = ({ sectionData }) => {
 	switch (type) {
 		case 'RICH':
 			return <SectionRichText data={data} id={id} />;
-
+		case 'PROFILE_CARD':
+			return <SectionProfilesCards data={data} id={id} />;
 		default:
 			return null;
 	}
