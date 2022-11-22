228660d6dfd8be1d1e7dea647883eca585ebb17e
Shabdan <shabdan@mancho.dev>
2022-11-22 15:04:37 +0600

    created hook for section

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,11 @@
 export const animateOptions = {
 	offscreen: {
-		y: 300
+		x: -300,
+		opacity: 0
 	},
 	onscreen: {
-		y: 0,
+		x: 0,
+		opacity: 1,
 		transition: {
 			type: 'spring',
 			bounce: 0.4,
