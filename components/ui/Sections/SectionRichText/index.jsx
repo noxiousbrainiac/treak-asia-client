c303ec2b939178712540516b4e84d0351efd152b
Shabdan <shabdan@mancho.dev>
2022-11-22 15:00:45 +0600

    updated

--- components/ui/Sections/SectionRichText/index.jsx
+++ components/ui/Sections/SectionRichText/index.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Container from 'components/ui/Container';
 import useSectionProps from './useSectionProps';
-import styles from './styles.module.scss';
 
 const SectionRichText = (props) => {
-	const { richText, options, id } = useSectionProps(props);
+	const { richText, options, className } = useSectionProps(props);
 
 	return (
-		<section id={id} className={styles.richTextSection}>
+		<section className={className}>
 			<Container>{documentToReactComponents(richText, options)}</Container>
 		</section>
 	);
